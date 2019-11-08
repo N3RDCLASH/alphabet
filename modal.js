c27f9f5b4e3db35e76d6dd922e0a0f2929d05055
Joel Naarendorp <35632308+N3RDCLASH@users.noreply.github.com>
2019-11-07 21:30:20 -0300

    updated landing page & first game screen

---
+++ modal.js
@@ -0,0 +1,20 @@
+function showModal(id) {
+      var modal = document.getElementById(id)
+    
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName(`close${id}`)[0]
+      var video = modal.getElementsByTagName("video")[0]
+
+      modal.style.display = "block";
+      span.onclick = function () {
+        video.pause()
+        modal.style.display = "none";
+      }
+      window.onclick = function (event) {
+        if (event.target == modal) {
+          video.pause()
+          modal.style.display = "none";
+        }
+      }
+      
+    }
