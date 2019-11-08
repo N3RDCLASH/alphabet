87ba8f7d96c0d17c07d70a5bef6eb82912695c8b
Joel Naarendorp <35632308+N3RDCLASH@users.noreply.github.com>
2019-11-08 10:41:13 -0300

    game UI update

--- modal.js
+++ modal.js
@@ -4,8 +4,9 @@ function showModal(id) {
       // Get the <span> element that closes the modal
       var span = document.getElementsByClassName(`close${id}`)[0]
       var video = modal.getElementsByTagName("video")[0]
-
+      video.play()
       modal.style.display = "block";
+      
       span.onclick = function () {
         video.pause()
         modal.style.display = "none";
